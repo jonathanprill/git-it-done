3051738905270bbcf10b265c5645f26333c08f3c
jonyprill <jonathantprill@gmail.com>
2022-01-26 17:21:32 -0600

    added buttons and searches for html css and js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,11 @@
+var languageButtonsEl = document.querySelector("#language-buttons")
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+
+
 var getUserRepos = function(user) {
     //format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -19,11 +27,6 @@ var getUserRepos = function(user) {
     });
 };
 
-
-var userFormEl = document.querySelector("#user-form");
-var nameInputEl = document.querySelector("#username");
-var repoContainerEl = document.querySelector("#repos-container");
-var repoSearchTerm = document.querySelector("#repo-search-term");
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -93,3 +96,35 @@ var displayRepos = function(repos, searchTerm) {
         repoContainerEl.appendChild(repoEl);
     }
 };
+
+
+
+//method of getting and sorting particualr search results 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: Github User not found');
+        }
+    });
+};
+
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language")
+    
+    if (language) {
+        getFeaturedRepos(language);
+
+        //clear old content
+        repoContainerEl.textContent = "";
+    }
+}
+
+
+//event listener TO THREE BUTTONS that will call a new button click handler function
+languageButtonsEl.addEventListener("click", buttonClickHandler);
